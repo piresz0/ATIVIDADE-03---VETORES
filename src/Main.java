9d6f16f42f3412b99454e133f2ce686e90ba8a91
piresz0 <renan090604@edu.unipar.br>
2024-09-05 07:30:26 -0300

    a

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] votos = new int[5];
+        votos[0] = 10;
+        votos[1] = 20;
+        votos[2] = 30;
+        votos[3] = 40;
+        votos[4] = 50;
+
+        System.out.println(votos[2]);
+
+    }
+}
